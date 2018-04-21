79ecc5e435b386b27764b635090e4490fee07ba4
Stanley Ung <stanleyung167@gmail.com>
2018-04-21 01:40:24 -0700

    Prototype

---
+++ InfiniteRunner/Assets/_Scripts/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject ground;
+
+    private void Start()
+    {
+        Spawn();
+    }
+    
+    void Spawn()
+    {
+        Instantiate(ground, transform.position, Quaternion.identity);
+        InvokeRepeating("Spawn", 0.0f, 0.5f);
+    }
+}
